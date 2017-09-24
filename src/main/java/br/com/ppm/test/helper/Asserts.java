9262bc8c01a1a495f55f4256acf7a90c0a06348a
Pedro Oliveira <pedro.oliveira20@gmail.com>
2017-09-24 01:43:52 -0400

    Increment Javadoc

--- src/main/java/br/com/ppm/test/helper/Asserts.java
+++ src/main/java/br/com/ppm/test/helper/Asserts.java
@@ -7,7 +7,7 @@ import org.hamcrest.Matcher;
  *
  * @author pedrotoliveira
  */
-public interface Asserts<ReturnType> {
+public interface Asserts<Return> {
 
     /**
      * Assert the return object equal to a object.
@@ -15,7 +15,7 @@ public interface Asserts<ReturnType> {
      * @param expected the expected object value.
      * @return the return object wrapper.
      */
-    ReturnObjectWrapper<ReturnType> assertEqualTo(ReturnType expected);
+    ReturnWrapper<Return> assertEqualTo(Return expected);
 
     /**
      * Assert the result of method execution object equal to a object.
@@ -23,84 +23,101 @@ public interface Asserts<ReturnType> {
      * @param expected the expected object value.
      * @return the return object wrapper.
      */
-    ReturnObjectWrapper<ReturnType> resultIsEqualTo(ReturnType expected);
+    ReturnWrapper<Return> resultIsEqualTo(Return expected);
 
     /**
      * Assert the return object to the selected matcher.
-     * <p/>
-     * @param matcher a macther
-     * <p/>
+     *
+     * @param matcher a Matcher <code>org.hamcrest.Matcher</code>
      * @return the return object wrapper
+     * @see org.hamcrest.CoreMatchers
+     * @see org.hamcrest.MatcherAssert
      */
-    ReturnObjectWrapper<ReturnType> assertReturn(Matcher<? super ReturnType> matcher);
+    ReturnWrapper<Return> assertReturn(Matcher<? super Return> matcher);
 
     /**
-     * Assert a field of return object to a matcher.
-     * <p/>
-     * Asserts a field of the object, example: assertReturnField("field1", equalTo("one"));
-     * <p/>
-     * @param field the field
-     * @param matcher a macther
-     * <p/>
+     * Assert a field of return object to a matcher.<br/>
+     *
+     * Asserts a field of the object, example: <code>assertReturnField("field1", equalTo("one"));</code>
+     *
+     * @param field the field name
+     * @param matcher a Matcher <code>org.hamcrest.Matcher</code>
      * @return the return object wrapper
+     * @see org.hamcrest.CoreMatchers
+     * @see org.hamcrest.MatcherAssert
      */
-    ReturnObjectWrapper<ReturnType> assertReturnField(String field, Matcher<?> matcher);
+    ReturnWrapper<Return> assertReturnField(String field, Matcher<?> matcher);
 
     /**
-     * Assert a field of the return object equal to the expected object.
-     * <p/>
-     * Asserts the fields of the object, example: assertEqualToReturnField("field1", "one");
-     * <p/>
+     * Assert a field of the return object equal to the expected object.<br/>
+     *
+     * Asserts the fields of the object, example: <code>assertEqualToReturnField("field1", "one");</code>
+     *
      * @param field the field
      * @param expected the expected object to assert
-     * <p>
      * @return the return object wrapper
      */
-    ReturnObjectWrapper<ReturnType> assertEqualToReturnField(String field, Object expected);
+    ReturnWrapper<Return> assertEqualToReturnField(String field, Object expected);
 
     /**
-     * Assert returnObject fields.
-     * <p/>
-     * Asserts the fields of the object, example: assertReturnFields("field1", equalTo("one"),
-     * "field2", equalTo("two"));
-     * <p/>
+     * Assert returnObject fields.<br/>
+     *
+     * Asserts the fields of the object,
+     * <code>example: assertReturnFields("field1", equalTo("one"), "field2", equalTo("two"));</code>
+     *
      * @param field the field
-     * @param matcher the matcher
+     * @param matcher a Matcher <code>org.hamcrest.Matcher</code>
      * @param additionalKeyMatcherPairs the additional key matcher pairs
-     * <p/>
      * @return the return object wrapper
+     * @see org.hamcrest.CoreMatchers
+     * @see org.hamcrest.MatcherAssert
      */
-    ReturnObjectWrapper<ReturnType> assertReturnFields(String field, Matcher<?> matcher, Object... additionalKeyMatcherPairs);
+    ReturnWrapper<Return> assertReturnFields(String field, Matcher<?> matcher, Object... additionalKeyMatcherPairs);
 
     /**
-     * Assert equalTo returnObject fields.
-     * <p/>
-     * Asserts the fields of the object, example: assertEqualToReturnFields("field1", "one",
-     * "field2", "two");
-     * <p/>
+     * Assert equalTo returnObject fields.<br/>
+     *
+     * Asserts the fields of the object,
+     * <code> example: assertEqualToReturnFields("field1", "one", "field2", "two");</code>
+     *
      * @param field the field
      * @param expected the object to match
      * @param additionalKeyMatcherPairs the additional key matcher pairs
-     * <p/>
      * @return the return object wrapper
      */
-    ReturnObjectWrapper<ReturnType> assertEqualToReturnFields(String field, Object expected, Object... additionalKeyMatcherPairs);
+    ReturnWrapper<Return> assertEqualToReturnFields(String field, Object expected, Object... additionalKeyMatcherPairs);
 
     /**
-     * Assert that.
+     * Asserts that <code>actual</code> satisfies the condition specified by <code>matcher</code>. If not, an
+     * {@link AssertionError} is thrown with the reason and information about the matcher and failing value. Example:
      *
-     * @param <ReturnType> the generic type
-     * @param methodCall the method call
-     * @param matcher the matcher
+     * <pre>
+     *   assertThat(&quot;Help! Integers don't work&quot;, 0, is(1)); // fails:
+     *     // failure message:
+     *     // Help! Integers don't work
+     *     // expected: is &lt;1&gt;
+     *     // got value: &lt;0&gt;
+     *   assertThat(&quot;Zero is one&quot;, 0, is(not(1))) // passes
+     * </pre>
+     *
+     * <code>org.hamcrest.Matcher</code> does not currently document the meaning of its type parameter <code>T</code>.
+     * This method assumes that a matcher typed as <code>Matcher&lt;T&gt;</code> can be meaningfully applied only to
+     * values that could be assigned to a variable of type <code>T</code>.
+     *
+     * @param methodCall a method call that return a type of Return, defined in interface Type.
+     * @param matcher a Matcher <code>org.hamcrest.Matcher</code>
+     * @return a Asserts<Return> Implementation
+     * @see org.hamcrest.CoreMatchers
+     * @see org.hamcrest.MatcherAssert
      */
-    Asserts<ReturnType> assertThat(ReturnType methodCall, Matcher<? super ReturnType> matcher);
+    Asserts<Return> assertThat(Return methodCall, Matcher<? super Return> matcher);
 
     /**
      * Assert True.
      *
      * @param methodCall the method call
      */
-    Asserts<ReturnType> assertTrue(boolean methodCall);
+    Asserts<Return> assertTrue(boolean methodCall);
 
     /**
      * Assert False.
@@ -108,7 +125,7 @@ public interface Asserts<ReturnType> {
      * @param methodCall
      * @return
      */
-    Asserts<ReturnType> assertFalse(boolean methodCall);
+    Asserts<Return> assertFalse(boolean methodCall);
 
     /**
      * Assert equal to.
@@ -117,5 +134,5 @@ public interface Asserts<ReturnType> {
      * @param methodCall the method call
      * @param expected the expected
      */
-    Asserts<ReturnType> assertEqualTo(ReturnType methodCall, Object expected);
+    Asserts<Return> assertEqualTo(Return methodCall, Object expected);
 }
